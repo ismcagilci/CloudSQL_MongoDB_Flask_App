cbd7b420a4bf7c0563249b766e0b7dca516f8c83
ismail cagilci <ismcagilci@gmail.com>
2021-09-14 15:04:10 +0300

    flask_app

---
+++ db.py
@@ -0,0 +1,17 @@
+
+from google.cloud.sql.connector import connector
+from pymongo import MongoClient
+
+def get_connect_cloudSql():
+    conn = connector.connect(
+    "istdsa-deneme:europe-west1:users-db",
+    "pymysql",
+    user="root",
+    password="****",
+    db='users',)
+    return conn
+
+def get_connect_mongoDB():
+    connect_db = "mongodb+srv://tonystank:i1024166@cluster0.kck47.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    cluster = MongoClient(connect_db)
+    return cluster
---
+++ rest.py
@@ -0,0 +1,32 @@
+#main.py
+from flask import Flask, jsonify, request
+from db import get_connect_cloudSql,get_connect_mongoDB
+
+app = Flask(__name__)
+
+@app.route('/insert', methods=['POST', 'GET'])
+def add_user_cloud():
+    if request.method == 'POST':
+        conn = get_connect()
+        cnx = conn.cursor()
+        username = request.form['username']
+        password = request.form['password']
+        cnx.execute("INSERT INTO user VALUES("+username+",PASSWORD("+password+"))")
+        return "<p>Succesful!</p>"
+    elif request.method == "GET":
+        return "<p>Hello, World!</p>"
+
+@app.route('/insertMongo', methods=['POST', 'GET'])
+def add_user_mongo():
+    if request.method == 'POST':
+        conn = get_connect_mongoDB()
+        myDB = conn["airbnbDB"]
+        mycol = myDB["airbnb"]
+        rawdata = request.json
+        mycol.insert_one(rawdata)
+        return "<p>Succesful!</p>"
+    elif request.method == "GET":
+        return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run()
